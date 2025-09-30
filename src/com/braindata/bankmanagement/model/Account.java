26e3879b232bec7b42004cf9b7cde4b68c2d47ab
kartik <kartikpawar916@gmail.com>
2025-09-30 17:12:43 +0530

    commit

---
+++ src/com/braindata/bankmanagement/model/Account.java
@@ -0,0 +1,54 @@
+package com.braindata.bankmanagement.model;
+
+public class Account {
+
+	private int accNo;
+	private String name;
+	private Long mobNo;
+	private Long adharNo;
+	private String gender;
+	private int age;
+	private double balance;
+	public int getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(int accNo) {
+		this.accNo = accNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getMobNo() {
+		return mobNo;
+	}
+	public void setMobNo(Long mobNo) {
+		this.mobNo = mobNo;
+	}
+	public Long getAdharNo() {
+		return adharNo;
+	}
+	public void setAdharNo(Long adharNo) {
+		this.adharNo = adharNo;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+}
